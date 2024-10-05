e3d3475977653d22bb77c80e73c31621c9a3e6d8
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2024-10-05 11:02:54 +0700

    Create: emums status type cubit

---
+++ lib/enums/status_type.dart
@@ -0,0 +1,12 @@
+enum StatusType { init, loading, loaded, error, loadmore }
+
+enum CubitStatus {
+  idle,
+  assigning,
+  accepted,
+  inProcess,
+  confirming,
+  paying,
+  completed,
+  cancelled,
+}
